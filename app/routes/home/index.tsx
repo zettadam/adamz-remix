6cbb332fdfd21aa6ef98049efb6d0281241040d3
Adam Ziolkowski <zett.adam@gmail.com>
2024-08-23 18:50:40 -0500

    Add content to home page

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from '@remix-run/node'
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
+
+import indexStylesheetHref from './index.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: indexStylesheetHref },
+]
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,8 +15,41 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <p>Hello, world!</p>
+    <div className="home">
+      <div className="about-me">
+        <span>
+          <p>
+            Hi, I am{' '}
+            <b>
+              Adam{' '}
+              <a href="https://www.howtopronounce.com/ziolkowski">Ziolkowski</a>
+            </b>
+            .
+          </p>
+          <p>
+            I climb web mountains on weekdays. On Saturdays, I follow the
+            Dao—mostly the <em>not trying</em> part. Any other time, I rest on
+            my laurels and fritter hours away polishing my RPG character sheet.
+          </p>
+          <p>
+            A ranged barbarian…{' '}
+            <a href="https://en.wikipedia.org/wiki/Alignment_(Dungeons_%26_Dragons)#Chaotic_good">
+              chaotic good
+            </a>
+            , on a quest to make better websites*.
+          </p>
+          <p>* I make and consult on web apps from Monday to Friday.</p>
+        </span>
+      </div>
+
+      <blockquote>
+        <p className="quote">
+          Nobody climbs mountains for scientific reasons. Science is used to
+          raise money for the expeditions, but{' '}
+          <strong>you really climb for the hell of it.</strong>
+        </p>
+        <p className="author">Edmund Hillary</p>
+      </blockquote>
     </div>
   )
 }
